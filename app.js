ca67ec4e938e09286d123c7e9e6f09cffaa892e6
Midhun Muraleedharan <mastermidhunmuraleedharan@gmail.com>
2024-09-18 22:04:37 +0530

    Complete Challenge 5

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.get("/contact", (req,res)=>{
   });
 });
 
+app.get("/compose", (req,res)=>{
+  res.render("compose.ejs", {
+    contactContent: contactContent
+  });
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
